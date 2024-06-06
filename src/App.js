0396171eb1481cc4e90e976c7b999a2fb69b8c59
Mahaan N Bhat <102415089+TopRamen95@users.noreply.github.com>
2024-06-07 01:28:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "./Screens/Signin";
+import SignUp from "./Screens/Signup";
+import Home from "./Screens/Home";
+import Web3 from "web3";
 
 function App() {
+  const email = localStorage.getItem("email");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={email? <Navigate to="/Home" /> : <SignIn />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route path="/Home" element={email? <Home /> : <Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
